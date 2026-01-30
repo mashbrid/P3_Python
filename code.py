c9ed5e55c7ad91de36aa340dbc29f70632fbf8ef
mashbrid <mike.ashbridge@gmail.com>
2026-01-29 19:15:12 -0700

    Update code.py
    Grip and haptic data is good.  Need to format sliders and get one working.  Good Baseline.

--- code.py
+++ code.py
@@ -10,11 +10,15 @@ from adafruit_ble import BLERadio
 from adafruit_ble.advertising.standard import ProvideServicesAdvertisement
 from adafruit_ble.services.nordic import UARTService
 
-# Setup FSR and Haptic
-fsr_in = AnalogIn(board.A3)  # Pin 3 analog input from FSR
-motor_pwm = pwmio.PWMOut(board.D0, frequency=1000, duty_cycle=0)  # Motor on D5
+# -------------------------
+# SETUP FSR & HAPTIC
+# -------------------------
+fsr_in = AnalogIn(board.A3)
+motor_pwm = pwmio.PWMOut(board.D0, frequency=1000, duty_cycle=0)
 
-# Setup Accelerometer
+# -------------------------
+# SETUP ACCELEROMETER
+# -------------------------
 imupwr = digitalio.DigitalInOut(board.IMU_PWR)
 imupwr.direction = digitalio.Direction.OUTPUT
 imupwr.value = True
@@ -22,71 +26,79 @@ time.sleep(0.1)
 imu_i2c = busio.I2C(board.IMU_SCL, board.IMU_SDA)
 sensor = LSM6DS3TRC(imu_i2c)
 
-# Initialize BLE radio
+# -------------------------
+# BLE RADIO
+# -------------------------
 ble = BLERadio()
 uart = UARTService()
 advertisement = ProvideServicesAdvertisement(uart)
 ble.name = "Paddle003"
 
-# set up on board LED colors
+# -------------------------
+# ONBOARD LEDS
+# -------------------------
 ledred = digitalio.DigitalInOut(board.LED_RED)
 ledred.direction = digitalio.Direction.OUTPUT
-ledred.value = True  # turn off red LED
+ledred.value = True
+
 ledblue = digitalio.DigitalInOut(board.LED_BLUE)
 ledblue.direction = digitalio.Direction.OUTPUT
-ledblue.value = True  # turn off blue LED
+ledblue.value = True
+
 ledgreen = digitalio.DigitalInOut(board.LED_GREEN)
 ledgreen.direction = digitalio.Direction.OUTPUT
-ledgreen.value = True  # turn off green LED
+ledgreen.value = True
 
-# values to blink LEDs when connected
 BLINK_ON_DURATION = 0.05
 BLINK_OFF_DURATION = 5
 LAST_BLINK_TIME = -1
 
-step_now = 1  # initialize variable
+step_now = 1  # initialize step
 
-# Load variables from NVM
+# -------------------------
+# LOAD VARIABLES FROM NVM
+# -------------------------
 fsr_on = microcontroller.nvm[0] == 1
-print("fsr_0n:", (fsr_on))
+print("fsr_on:", fsr_on)
+
 step1 = (microcontroller.nvm[1] << 8) | microcontroller.nvm[2]
-print("step1:", (step1))
-time.sleep(.1)
 step10 = (microcontroller.nvm[3] << 8) | microcontroller.nvm[4]
-print("step10:", (step10))
+
 haptic2 = microcontroller.nvm[5]
-print("haptic2:", (haptic2))
 haptic3 = microcontroller.nvm[6]
-print("haptic3:", (haptic3))
 haptic4 = microcontroller.nvm[7]
-print("haptic4:", (haptic4))
 haptic5 = microcontroller.nvm[8]
-print("haptic5:", (haptic5))
 haptic6 = microcontroller.nvm[9]
-print("haptic6:", (haptic6))
 haptic7 = microcontroller.nvm[10]
-print("haptic7:", (haptic7))
 haptic8 = microcontroller.nvm[11]
-print("haptic8:", (haptic8))
 haptic9 = microcontroller.nvm[12]
-print("haptic9:", (haptic9))
 haptic10 = microcontroller.nvm[13]
-print("haptic10:", (haptic10))
+
 xangle_on = microcontroller.nvm[14] == 1
-print("xangle_on:", (xangle_on))
-#  xangle is the angle when haptic is triggered (0 to 10)
 xangle = microcontroller.nvm[15] * -1
-print("xangle:", (xangle))
-#  xhaptic is the strength of the haptic (25, 50, 75, or 100)
 xhaptic = microcontroller.nvm[16]
-print("xhaptic:", (xhaptic))
-xtime = microcontroller.nvm[17] * .1
-print("xtime:", (xtime))
+xtime = microcontroller.nvm[17] * 0.1
 
-# Variables to track X state
-x_negative_start_time = None  # Timestamp when X becomes negative
+# Print NVM values for debugging
+print("step1:", step1)
+print("step10:", step10)
+print("haptic2:", haptic2)
+print("haptic3:", haptic3)
+print("haptic4:", haptic4)
+print("haptic5:", haptic5)
+print("haptic6:", haptic6)
+print("haptic7:", haptic7)
+print("haptic8:", haptic8)
+print("haptic9:", haptic9)
+print("haptic10:", haptic10)
+print("xangle_on:", xangle_on)
+print("xangle:", xangle)
+print("xhaptic:", xhaptic)
+print("xtime:", xtime)
 
-# Set values for FSR resistance to each step
+# -------------------------
+# CALCULATE FSR STEPS
+# -------------------------
 fsr_increment = (step10 - step1) / 9
 step2 = step1 + fsr_increment
 step3 = step2 + fsr_increment
@@ -97,142 +109,125 @@ step7 = step6 + fsr_increment
 step8 = step7 + fsr_increment
 step9 = step8 + fsr_increment
 
-#  Flash green LED twice and run haptice twice to signal startup OK
-ledgreen.value = False
-motor_pwm.duty_cycle = int(10 * 65535 / 100)  # 10% intensity
-time.sleep(0.2)
-ledgreen.value = True
-motor_pwm.duty_cycle = 0  # Haptic off
-time.sleep(0.2)
-ledgreen.value = False
-motor_pwm.duty_cycle = int(10 * 65535 / 100)  # 10% intensity
-time.sleep(0.2)
-ledgreen.value = True
-motor_pwm.duty_cycle = 0  # Haptic off
+# -------------------------
+# DOUBLE BUZZ & GREEN LED STARTUP
+# -------------------------
+for _ in range(2):
+    ledgreen.value = False
+    motor_pwm.duty_cycle = int(10 * 65535 / 100)
+    time.sleep(0.2)
+    ledgreen.value = True
+    motor_pwm.duty_cycle = 0
+    time.sleep(0.2)
 
-# Global variables
-flashcounter = 1  # Green LED heartbeat flash if Pic 1 for several cycles
-
+# -------------------------
+# HELPER FUNCTIONS
+# -------------------------
 def get_voltage(pin):
     return pin.value
 
-def show_xyz():
-    x, y, z = sensor.acceleration  # Acceleration in m/s^2
-    print("x is ", (x))
-    print("y is ", (y))
-    print("z is ", (z))
-    time.sleep(0.1)
-
+x_negative_start_time = None
 def check_xangle():
-    global x_negative_start_time  # Declare x_negative_start_time as global
-    # Read accelerometer data
-    x, y, z = sensor.acceleration  # Acceleration in m/s^2
-
-    # Check if X is less than 0
+    global x_negative_start_time
+    x, y, z = sensor.acceleration
     if x < xangle:
         if x_negative_start_time is None:
-            # Start tracking time when X first becomes negative
             x_negative_start_time = time.monotonic()
         elif time.monotonic() - x_negative_start_time >= xtime:
-            # If X has been negative for the threshold duration, pulse the haptic
-            print("X negative > 1 second, trigger haptic.")
-            motor_pwm.duty_cycle = int(xhaptic * 65535 / 100)
-            time.sleep(0.08)
-            motor_pwm.duty_cycle = 0  # Haptic off
-            time.sleep(0.08)
-            motor_pwm.duty_cycle = int(xhaptic * 65535 / 100)
-            time.sleep(0.08)
-            motor_pwm.duty_cycle = 0  # Haptic off
-            time.sleep(0.08)
-            motor_pwm.duty_cycle = int(xhaptic * 65535 / 100)
-            time.sleep(0.08)
-            motor_pwm.duty_cycle = 0  # Haptic off
-            time.sleep(0.08)
-            x_negative_start_time = None  # Reset after pulsing
+            for _ in range(3):
+                motor_pwm.duty_cycle = int(xhaptic * 65535 / 100)
+                time.sleep(0.08)
+                motor_pwm.duty_cycle = 0
+                time.sleep(0.08)
+            x_negative_start_time = None
     else:
-        # Reset tracking if X is not negative
         x_negative_start_time = None
 
+flashcounter = 1
 def check_step():
-    time.sleep(0.05)
     global flashcounter, step_now
-    fsr_value = get_voltage(fsr_in)
-    if fsr_value <= step1:  # step1 always off
+
+    fsr_value = get_voltage(fsr_in)  # always read FSR
+    
+    if not fsr_on:
+        motor_pwm.duty_cycle = 0
+        
+    if fsr_value <= step1:
         step_now = 1
-        # print(f"Step {step_now}: {get_voltage(fsr_in)}")
-        motor_pwm.duty_cycle = 0  # Haptic off
-        flashcounter = flashcounter + 1  # increment flashcounter
-    if fsr_value > step1 and fsr_value <= step2:  # step2
+        if fsr_on:
+            motor_pwm.duty_cycle = 0
+        flashcounter += 1
+    elif fsr_value <= step2:
         step_now = 2
         if fsr_on:
             motor_pwm.duty_cycle = int(haptic2 * 65535 / 100)
-        else:
-            motor_pwm.duty_cycle = 0
         flashcounter = 1
-    if fsr_value > step2 and fsr_value <= step3:  # step3
+    elif fsr_value <= step3:
         step_now = 3
         if fsr_on:
             motor_pwm.duty_cycle = int(haptic3 * 65535 / 100)
-        else:
-            motor_pwm.duty_cycle = 0
         flashcounter = 1
-    if fsr_value > step3 and fsr_value <= step4:  # step4
+    elif fsr_value <= step4:
         step_now = 4
         if fsr_on:
             motor_pwm.duty_cycle = int(haptic4 * 65535 / 100)
-        else:
-            motor_pwm.duty_cycle = 0
         flashcounter = 1
-    if fsr_value > step4 and fsr_value <= step5:  # step5
+    elif fsr_value <= step5:
         step_now = 5
         if fsr_on:
             motor_pwm.duty_cycle = int(haptic5 * 65535 / 100)
-        else:
-            motor_pwm.duty_cycle = 0
         flashcounter = 1
-    if fsr_value > step5 and fsr_value <= step6:  # step6
+    elif fsr_value <= step6:
         step_now = 6
         if fsr_on:
             motor_pwm.duty_cycle = int(haptic6 * 65535 / 100)
-        else:
-            motor_pwm.duty_cycle = 0
         flashcounter = 1
-    if fsr_value > step6 and fsr_value <= step7:  # step7
+    elif fsr_value <= step7:
         step_now = 7
         if fsr_on:
             motor_pwm.duty_cycle = int(haptic7 * 65535 / 100)
-        else:
-            motor_pwm.duty_cycle = 0
         flashcounter = 1
-    if fsr_value > step7 and fsr_value <= step8:  # step8
+    elif fsr_value <= step8:
         step_now = 8
         if fsr_on:
             motor_pwm.duty_cycle = int(haptic8 * 65535 / 100)
-        else:
-            motor_pwm.duty_cycle = 0
         flashcounter = 1
-    if fsr_value > step8 and fsr_value <= step9:  # step9
+    elif fsr_value <= step9:
         step_now = 9
         if fsr_on:
             motor_pwm.duty_cycle = int(haptic9 * 65535 / 100)
-        else:
-            motor_pwm.duty_cycle = 0
         flashcounter = 1
-    if fsr_value > step9:  # step10
+    else:
         step_now = 10
         if fsr_on:
             motor_pwm.duty_cycle = int(haptic10 * 65535 / 100)
-        else:
-            motor_pwm.duty_cycle = 0
         flashcounter = 1
-    if (flashcounter) > 3000:  # flash LED if sitting in step1
-        ledgreen.value = False  # turns LED on
+
+    if flashcounter > 3000:
+        ledgreen.value = False
         time.sleep(0.1)
-        ledgreen.value = True  # turns LED off
-        flashcounter = 1  # reset LED flashcounter
-# save_variables_to_nvm()
+        ledgreen.value = True
+        flashcounter = 1
+
+# -------------------------
+# TURN ON STEP FOR APP
+# -------------------------
+def get_turn_on_step():
+    if haptic2 > 0: return 2
+    if haptic3 > 0: return 3
+    if haptic4 > 0: return 4
+    if haptic5 > 0: return 5
+    if haptic6 > 0: return 6
+    if haptic7 > 0: return 7
+    if haptic8 > 0: return 8
+    if haptic9 > 0: return 9
+    if haptic10 > 0: return 10
+    return 0
+
+# -------------------------
+# MAIN LOOP
+# -------------------------
 while True:
-    # Advertise BLE if not connected
     if not ble.connected:
         ble.start_advertising(advertisement)
         print("Advertising...")
@@ -240,7 +235,6 @@ while True:
             check_step()
             if xangle_on:
                 check_xangle()
-            # show_xyz()
             time.sleep(0.05)
         ble.stop_advertising()
         print("Connected!")
